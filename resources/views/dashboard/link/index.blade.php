66c3aa05fcb3c193077f514b528af2d98769e383
Mahdi Naderian <naderian.mahdi@gmail.com>
2019-08-28 11:28:04 +0430

    completed index view for links section

--- resources/views/dashboard/link/index.blade.php
+++ resources/views/dashboard/link/index.blade.php
@@ -26,19 +26,100 @@
                             <td>
                                 <select class="item_schema" data-id="{{ $link->id }}" data-original-schema="{{ $link->item_schema_id }}">
                                     <option value="" disabled selected>Select</option>
+									@foreach($itemSchemas as $item)
+										<option value="{{ $item->id }}" {{ $item->id == $link->item_schema_id ? "selected" : "" }}>{{ $item->title }}</option>
+									@endforeach
                                 </select>
+								<button type="button" class="btn btn-info btn-sm btn-apply" style="display:none">Apply</button>
                             </td>
+							<td>
+								@if ($link->item_schema_id != "" && $link->main_filter_selector != "")
+									<button type="button" class="btn btn-primary btn-scrape" title="pull the latest items">Scrape <i class="glyphicon glyphicon-repeat fast-right-spinner" style="display:none"></i></button>
+									@else
+										<span style="color: red">fill main filter selector and item schema first</span>
+								@endif
+							</td>
+							<td>
+								<a href="{{ url('dashboard/links/' . $link->id . '/edit') }}"><i class="glyphicon glyphicon-edit"></i></a>
+							</td>
 						</tr>
 					@endforeach
 				</table>
-				@if(count($websites) > 0)
+				@if(count($links) > 0)
 					<div class="pagination">
-						{{ $websites->render() }}
+						{{ $links->render() }}
 					</div>
 				@endif
 				@else
-					<i>No websites found</i>
+					<i>No links found</i>
 				@endif
 		</div>
 	</div>
+@endsection
+
+@section('script')
+	<script>
+		$(function() {
+			$("select.item_schema").change(function() {
+				if($(this).val() != $(this).attr("data-original-schema")) {
+					$(this).siblings('.btn-apply').show();
+				}
+			});
+
+			$('.btn-apply').click(function() {
+				var btn = $(this);
+				var tRowId = $(this).parents("tr").attr("data-id");
+				var schema_id = $(this).siblings('select').val();
+				$.ajaxSetup({
+					headers: {
+						'X-XSRF-TOKEN': "{{ csrf_token() }}"
+					}
+				});
+				$.ajax({
+					url: "{{ url('dashboard/links/set-item-schema') }}",
+					data: {
+						link_id: tRowId,
+						item_schema_id: schema_id,
+						_token: "{{ csrf_token() }}",
+						_method: "patch"
+					},
+					method: "post",
+					dataType: "json",
+					success: function(response) {
+						alert(response.msg);
+						btn.hide();
+					}
+				});
+			});
+
+			$(".btn-scrape").click(function() {
+				var btn = $(this);
+				btn.find(".fast-right-spinner").show();
+				btn.prop("disabled", true);
+				var tRowId = $(this).parents("tr").attr("data-id");
+				$.ajaxSetup({
+					headers: {
+						'X-XSRF-TOKEN': "{{ csrf_token() }}"
+					}
+				});
+				$.ajax({
+					url: "{{ url('dashboard/links/scrape') }}",
+					data: {
+						link_id: tRowId,
+						_token: "{{ csrf_token() }}"
+					},
+					method: "post",
+					dataType: "json",
+					success: function(response) {
+						if(response.status == 1) {
+							$(".alert").removeClass("alert-danger").addClass("alert-success").text(response.msg).show();
+						} else {
+							$(".alert").removeClass("alert-success").addClass("alert-danger").text(response.msg).show();
+						}
+						btn.find(".fast-right-spinner").hide();
+					}
+				});
+			});
+		});
+	</script>
 @endsection
