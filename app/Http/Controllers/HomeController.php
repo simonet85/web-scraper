0a5d894d049a936419cbe353c51c73f60c0eb42b
Mahdi Naderian <naderian.mahdi@gmail.com>
2019-08-28 12:04:56 +0430

    added home controller + view

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+use App\Article;
+
+class HomeController extends Controller
+{
+    
+    public function index()
+    {
+        // ...
+    }
+
+
+    public function getArticleDetails($id)
+    {
+        // ...
+    }
+
+
+    public function getCategory($id)
+    {
+        // ...
+    }
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Articles</h2>
+            ....
+        </div>
+    </div>
+@endsection
