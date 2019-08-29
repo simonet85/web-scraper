4214709d5770035eaddda633ba518ae454293215
Mahdi Naderian <naderian.mahdi@gmail.com>
2019-08-29 14:11:05 +0430

    added two patch routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/category/{id}', 'HomeController@getCategory');
 Route::group(['prefix' => 'dashboard'], function () {
 	Route::resource('/websites', 'WebsitesController');
 	Route::resource('/categories', 'CategoriesController');
+	Route::patch('/links/set-item-schema', 'LinksController@setItemSchema');
+	Route::patch('/links/scrape', 'LinksController@scrape');
 	Route::resource('/links', 'LinksController');
 	Route::resource('/item-schema', 'ItemSchemaController');
 	Route::resource('/articles', 'ArticlesController');
