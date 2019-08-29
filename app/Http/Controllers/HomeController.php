b79121130c37c7fcbdbd8f09ca58a18fe6e0fe06
Mahdi Naderian <naderian.mahdi@gmail.com>
2019-08-29 10:00:08 +0430

    updated HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,19 +11,23 @@ class HomeController extends Controller
     
     public function index()
     {
-        // ...
+        $articles = Article::orderBy('id', 'DESC')->paginate(20);
+        return view('home')->withArticles($articles);
     }
 
 
     public function getArticleDetails($id)
     {
-        // ...
+        $article = Article::find($id);
+        return view('details')->withArticles($article);
     }
 
 
     public function getCategory($id)
     {
-        // ...
+        $category = Category::find($id);
+        $articles = Article::where('category_id', $id)->orderBy('id', 'DESC')->paginate(20);
+        return view('category')->withArticles($articles)->withCategory($category);
     }
 
 }
